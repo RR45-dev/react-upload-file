f81c05a384940d4e910d1cb24a65758049a21cc4
RR45-dev <id>
2025-06-12 03:36:21 -0600

    added post method

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 
 const API_BASE = 'https://file-upload-app-cedtcedbfqfxgkg0.canadaeast-01.azurewebsites.net'; 
 
+
 export const uploadFile = (file, onUploadProgress) => {
   const fd = new FormData();
   fd.append('file', file);
-  return axios.post('/api/files/upload', fd, { onUploadProgress });
+  return axios.post(
+    `${API_BASE}/api/files/upload`,
+    fd,
+    { onUploadProgress }
+  );
 };
-
 export const listFiles = (page = 1, pageSize = 10) =>
   axios.get(`${API_BASE}/api/files/viewall`, {
     params: { page, pageSize }
@@ -16,5 +20,9 @@ export const listFiles = (page = 1, pageSize = 10) =>
 export const parseImage = (file, onUploadProgress) => {
   const fd = new FormData();
   fd.append('image', file);
-  return axios.post('/api/idcard/parse', fd, { onUploadProgress });
+  return axios.post(
+    `${API_BASE}/api/idcard/parse`,
+    fd,
+    { onUploadProgress }
+  );
 };
